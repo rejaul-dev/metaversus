0e6e765a2ca02d5767b3bf55c95df972ad053740
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-12-26 21:15:18 +0800

    added hero section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,7 +17,6 @@ const Navbar = () => (
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white">METAVERSUS</h2>
       <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain cursor-pointer" />
     </div>
-    navbar
   </motion.nav>
 );
 
